1b9e494ed13b53bd14971c9a232a0db1ce411c80
Amaan Beg <amaancse90@gmail.com>
2026-06-04 10:04:31 +0530

    Add files via upload

---
+++ sumOfNumbersFrom1ToN.cpp
@@ -0,0 +1,17 @@
+// Find Sum of Numbers from 1 TO n
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        sum += i;
+    }
+    cout << sum << endl;
+
+    return 0;
+}
