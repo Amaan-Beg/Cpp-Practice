a5743b528327a68d3efe7074234e8df980bd0058
Amaan Beg <amaancse90@gmail.com>
2026-06-10 10:10:44 +0530

    Update findSmallestNumArray.cpp

--- findSmallestNumArray.cpp
+++ findSmallestNumArray.cpp
@@ -1,4 +1,4 @@
-// Find the smallest  number in an arrey.
+// Find the smallest  number in an array with its index.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -7,15 +7,19 @@ int main(){
     int arr[] = {12,45,8,0,-23,99};
     int size = 6;
     int smallest = INT_MAX;
+    int index = 0;
 
-    for (int i = 0; i <= size; i++)
+    for (int i = 0; i < size; i++)
     {
         if (arr[i] < smallest)
         {
             smallest = arr[i];
+            index = i;
         }
         
     }
     cout << "Smallest = " << smallest << endl;
+    cout << "Index = " << index << endl;
 
+    return 0;
 }
