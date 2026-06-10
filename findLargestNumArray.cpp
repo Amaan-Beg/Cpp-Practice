589ac2b53300677ddf215aebad694e0abc25e1a2
Amaan Beg <amaancse90@gmail.com>
2026-06-10 10:13:29 +0530

    Update findLargestNumArray.cpp

--- findLargestNumArray.cpp
+++ findLargestNumArray.cpp
@@ -1,4 +1,4 @@
-// Find the largest  number in an arrey.
+// Find the largest  number in an array with its index.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -7,15 +7,20 @@ int main(){
     int arr[] = {12,45,8,0,-23,99};
     int size = 6;
     int largest = INT_MIN;
+    int index = 0;
 
     for (int i = 0; i <= size; i++)
     {
         if (arr[i] > largest)
         {
             largest = arr[i];
+            index = i;
         }
         
     }
     cout << "Largest = " << largest << endl;
+    cout << "Index = " << index << endl;
+
+    return 0;
 
 }
